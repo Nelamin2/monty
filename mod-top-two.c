f88b7c6d504c89eab0a46063e5e848e7ee510c2b
Nelamin2 <nidalelamin@gmail.com>
2023-12-14 15:44:34 -0800

    task9

--- mod-top-two.c
+++ mod-top-two.c
@@ -8,10 +8,21 @@
 void mod_top_two(stack_t **top, unsigned int line_number)
 {
 int result;
+int length;
+length = 0;
+stack_t mes;
 if (top == NULL || *top == NULL || (*top)->next == NULL)
 handle_error(8, line_number);
 if ((*top)->n == 0)
 handle_error(9, line_number);
+mes = *top;
+while (mes)
+{
+mes = mes->next;
+length++;
+}
+if (length < 2)
+handle_error(8, line_number)
 *top = (*top)->next;
 result = (*top)->n % (*top)->prev->n;
 (*top)->n = result;
